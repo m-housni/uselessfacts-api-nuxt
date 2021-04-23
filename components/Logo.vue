0461b07a8b920465c2189b799650ca005bbdc863
HOUSNI Mohamed <mohamed.housni@um5s.net.ma>
2021-04-23 10:59:08 +0000

    initial commit

---
+++ components/Logo.vue
@@ -0,0 +1,19 @@
+<template>
+  <img src="../assets/mypic.png" alt="" class="NuxtLogo">
+</template>
+
+<style>
+.NuxtLogo {
+  animation: 3s appear;
+  margin: auto;
+  border-radius: 50%;
+  height: 128px;
+  border: 5px blue solid;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
